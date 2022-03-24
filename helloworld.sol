4b910c46d836413c85859450371814c0e2af6055
dameeolawuyi <83360411+dameeolawuyi@users.noreply.github.com>
2022-03-24 05:22:20 -0700

    Create helloworld.sol

---
+++ helloworld.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+
+
+contract helloWorld{
+
+    address public owner;
+    string public greeter;
+    uint public value;
+
+    constructor (address _owner) { 
+
+    }
+
+    function getBalance(address _owner, uint _value)public view returns(uint ){
+    
+
+    }
+
+
+}
